9459c6041127a4a334567f773febe0c063a4a8e9
Edward-Magee <edward.mageev3@gmail.com>
2022-02-18 17:07:24 +1100

    Refactor the typing code

--- src/components/LoremProductionSystem.tsx
+++ src/components/LoremProductionSystem.tsx
@@ -7,25 +7,27 @@ export let type: () => void;
 
 const translateLatinToEnglish = (structurePart: StructurePart): string => {
    let english!: string;
-   switch (structurePart.wordCategory) {
-      case "noun": {
-         const x = structurePart.number === "singular" ? 0 : 1;
-         const y = ["nominative", "accusative", "genetive", "dative", "ablative"].indexOf(structurePart.case!);
+   // switch (structurePart.wordCategory) {
+   //    case "noun": {
+   const x = structurePart.number === "singular" ? 0 : 1;
+   const y = ["nominative", "accusative", "genetive", "dative", "ablative"].indexOf(structurePart.case!);
 
-         let word!: Word;
-         for (const loremPack of LOREM_PACKS) {
-            if (typeof loremPack.contents.words === "undefined") continue;
-            for (const currentWord of loremPack.contents.words) {
-               if (currentWord.latin === structurePart.latin) {
-                  word = currentWord;
-                  break;
-               }
-            }
+   let word!: Word;
+   for (const loremPack of LOREM_PACKS) {
+      if (typeof loremPack.contents.words === "undefined") continue;
+      for (const currentWord of loremPack.contents.words) {
+         if (currentWord.latin === structurePart.latin) {
+            word = currentWord;
+            break;
          }
-
-         english = word.caseTable[y][x];
       }
    }
+
+   console.log(structurePart);
+   console.log(word.caseTable, x, y);
+   english = word.caseTable[y][x];
+   //    }
+   // }
    return english;
 }
 
@@ -35,17 +37,40 @@ interface TranslationDict {
 const sentenceStructureToEnglish = (sentenceStructure: SentenceStructure): string => {
    const translationDict: TranslationDict = {};
 
-   // Look for any nouns in the sentence
-   
-   const structureParts: Array<[string, StructurePart]> = Object.entries(sentenceStructure.structure);
-   for (const [id, structurePart] of structureParts) {
-      let english!: string;
-      if (structurePart.wordCategory === "noun") {
-         english = translateLatinToEnglish(structurePart);
-      }
-      translationDict[Number(id)] = english;
+   const addToDict = (id: number, structurePart: StructurePart): void => {
+      const englishTranslation = translateLatinToEnglish(structurePart);
+      translationDict[id] = englishTranslation;
    }
 
+   // Look for any nouns in the sentence
+   
+   const structureParts: Array<[number, StructurePart]> = Object.entries(sentenceStructure.structure).map(([a, b]) => [Number(a), b]);
+   for (const [id, structurePart] of structureParts) {
+      if (structurePart.wordCategory === "noun") {
+         console.log(id);
+         addToDict(id, structurePart);
+
+         // See if the noun has a matching word
+         for (const pair of sentenceStructure.pairs) {
+            const idx = pair.indexOf(id);
+            console.log(typeof id);
+            console.log(pair, id);
+            if (idx !== -1) {
+               // console.log("Found pair!", pairWord, idx, pairID);
+               const pairID = pair[Math.abs(idx - 1)];
+               const pairWord = sentenceStructure.structure[pairID];
+               const pairStructurePart: StructurePart = {
+                  latin: pairWord.latin,
+                  wordCategory: pairWord.wordCategory,
+                  case: structurePart.case
+               };
+               addToDict(pairID, pairStructurePart);
+            }
+         }
+      }
+   }
+
+   console.log(translationDict);
    return "obama gaming";
 }
 
