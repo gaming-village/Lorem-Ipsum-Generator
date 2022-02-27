4df3eca4a11a11c9ba58789615eddb81b5f6eccf
eggpoison <edwrd.magee@gmail.com>
2022-02-27 12:49:17 +1100

    Potential fix

--- src/components/LoremProductionSystem.tsx
+++ src/components/LoremProductionSystem.tsx
@@ -315,9 +315,11 @@ const LoremProductionSystem = () => {
          // If a word has been completed, award lorem based on its value.
          console.log(currentSentence, currentIndex);
          if (wordEndingChars.includes(currentSentence[currentIndex + 1])) {
+            console.log("detected word completion.");
             const word = findWord(currentSentence, currentIndex);
-
+            console.log("word:", word);
             const wordValue = calculateWordValue(word.value);
+            console.log("value:", wordValue);
             Game.lorem += wordValue;
          }
 
